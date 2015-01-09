8d47788ebfc62a34d0d0effb148d47f130f5ee52
Kyle Oba <koba@mudphone.com>
2015-01-08 22:07:51 -1000

    Add Tuples.

--- PLAYGROUNDS/LearningSwiftBasics_Tuples.playground/section-2.swift
+++ PLAYGROUNDS/LearningSwiftBasics_Tuples.playground/section-2.swift
@@ -9,6 +9,31 @@ let (firstName, lastName, age) = rob
 firstName
 rob.1
 
+var person: (name: String, age: Int)
+person.name = "Rob"
+person.age = 21
 
 
+// As argument type
+func birth() -> (name: String, eyeColor: String) {
+  return ("George", "blue")
+}
+let p = birth()
+p.name
+p.eyeColor
+
+
+// As return type
+func pickNumberFromBag() -> (result: Int, isLast: Bool) {
+  return (Int(arc4random_uniform(10)), false)
+}
+pickNumberFromBag().0
+
+
+// Type Alias
+typealias human = (name: String, height: Double)
+func enroll(h: human) -> String {
+  return "\(h.name) is \(h.height) feet tall."
+}
+enroll(("Phil", 6.3))
 
