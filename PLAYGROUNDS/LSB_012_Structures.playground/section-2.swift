a6baf6a87bbd00268aac6ecc9012ea15d1125d3f
Kyle Oba <koba@mudphone.com>
2015-01-10 21:34:14 -1000

    Add Structures and rename to shorter playground files.

---
+++ PLAYGROUNDS/LSB_012_Structures.playground/section-2.swift
@@ -0,0 +1,68 @@
+import UIKit
+
+/*
+//  Structures
+/==============================*/
+
+
+/*--------------------------/
+//  Methods and Properties
+/--------------------------*/
+struct Circle {
+  // property
+  var radius: Double = 10.0
+  
+  // method
+  func area() -> Double {
+    return 3.14 * pow(radius, 2.0)
+  }
+  
+  // mutating method
+  mutating func scalex(factor: Double) {
+    radius *= factor
+  }
+  
+}
+
+var c = Circle()
+c.radius
+c.area()
+
+c.scalex(2.0) // only works if "Circle" instance is a `var`
+c.radius
+c.area()
+
+
+/*------------------------------------/
+// Automatically creates initializer
+/------------------------------------*/
+c = Circle(radius: 20.0)
+c.radius
+
+
+/*------------------------------------/
+// Initializer delegation
+/------------------------------------*/
+struct Square {
+  var side: Double
+
+  init(_ side: Double) {
+    self.side = side
+  }
+  
+  init() {
+    // call designated initializer
+    self.init(10.0)
+  }
+}
+
+let s1 = Square(20.0)
+s1.side
+
+let s2 = Square()
+s2.side
+
+
+
+
+
