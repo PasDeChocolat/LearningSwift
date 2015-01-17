ef9f9e2c14f0682ddbffc751b4cd3e738df638e1
Kyle Oba <koba@mudphone.com>
2015-01-16 17:12:18 -1000

    Add map, filter via reduce and currying.
    To suplement the Higher Order Functions section.

--- PLAYGROUNDS/LSB_B001_HigherOrderFunctions.playground/section-2.swift
+++ PLAYGROUNDS/LSB_B001_HigherOrderFunctions.playground/section-2.swift
@@ -306,20 +306,106 @@ reduce(words, "", +)
 /*------------------------------------/
 //  Map, via Reduce
 /------------------------------------*/
+func rmap<T, U>(xs: [T], f: T -> U) -> [U] {
+  return xs.reduce([], combine: { result, x in
+    return result + [f(x)]
+  })
+}
 
+func rmap2<T, U>(xs: [T], f: T -> U) -> [U] {
+  return xs.reduce([]) { $0 + [f($1)] }
+}
 
-
-
+let vPlusOne = rmap2(v) { $0 + 1 }
+v
+vPlusOne
 
 
 /*------------------------------------/
 //  Filter, via Reduce
 /------------------------------------*/
+func rfilter<T>(xs: [T], check: T -> Bool) -> [T] {
+  return xs.reduce([], combine: { result, x -> [T] in
+    return check(x) ? result + [x] : result
+  })
+}
+
+func rfilter2<T>(xs: [T], check: T -> Bool) -> [T] {
+  return xs.reduce([]) { check($1) ? $0 + [$1] : $0 }
+}
+
+let vEvens = rfilter2(v) { $0 % 2 == 0 }
+vEvens
+
 
 /*------------------------------------/
 //  Function Composition
 /------------------------------------*/
+// Allows composition of two functions.
+// You could think of this as a pipeline of two steps.
+infix operator >>> { associativity left }
+func >>> <A, B, C>(f: A -> B, g: B -> C) -> A -> C {
+  return { x in g(f(x)) }
+}
+
+// step 1
+func s1(x: Int) -> Int {
+  return x * 2
+}
+
+// step 2
+func s2(x: Int) -> Int {
+  return x + 10
+}
+
+// steps 1+2 combined
+let h = s1 >>> s2  // do step 1, then do step 2
+h(1)
+
 
 /*------------------------------------/
 //  Currying
 /------------------------------------*/
+func curry <A, B, C>(f: (A, B) -> C) -> A -> B -> C {
+  return { x in { y in f(x, y) } }
+}
+
+// step 1: multiply by x
+// step 2: add y
+// Note: Applying in reverse order is technically incorrect.
+func twoStepProc(x: Int, y: Int) -> Int {
+  return (1 * x) + y
+}
+
+let twoStepCurry = curry(twoStepProc)
+let step2 = twoStepCurry(2) // step 1: mult by 2
+step2(10) // step 2: add 10
+
+
+// A real world example
+func orderTotal(unitPrice: Double, quantity: Int) -> Double {
+  return unitPrice * Double(quantity)
+}
+
+// price of a ticket
+var ticketPrice = 11.99
+
+// create a function to calculate ticket order totals
+let ticketCalculator = curry(orderTotal)(ticketPrice)
+
+// find the price of 10 tickets
+ticketCalculator(10)
+
+
+/*------------------------------------/
+//  Curring by Declaration
+/------------------------------------*/
+func orderTotal(# unitPrice: Double)(quantity: Int) -> Double {
+  return unitPrice * Double(quantity)
+}
+let sandwichCalculator = orderTotal(unitPrice: 4.50)
+sandwichCalculator(quantity: 10)
+
+
+
+
