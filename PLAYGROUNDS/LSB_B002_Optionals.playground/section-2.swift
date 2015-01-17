8569549c3137bfaa1d0463bd3f758edfb38d2319
Kyle Oba <koba@mudphone.com>
2015-01-17 11:02:21 -1000

    Add "nil" case to test of `map` on optionals.

--- PLAYGROUNDS/LSB_B002_Optionals.playground/section-2.swift
+++ PLAYGROUNDS/LSB_B002_Optionals.playground/section-2.swift
@@ -12,12 +12,20 @@ import UIKit
 
 /*-----------------------------------------------/
 //  Map is also a Swift function on optionals.
+//
+//  Suggested Reading:
+//  http://www.objc.io/books/ (Chapter 5: Optionals)
+//  http://natashatherobot.com/swift-using-map-to-deal-with-optionals/
 /-----------------------------------------------*/
 let optional: Int? = 0
 let mapped = optional.map { x in x + 1 }
 optional!
 mapped!
 
+// If "nil" then `map` returns nil
+let optNil: Int? = nil
+let mappedNil = optNil.map { x in x + 1 }
+if mappedNil == nil { "mappedNil is nil" }
 
 
 
