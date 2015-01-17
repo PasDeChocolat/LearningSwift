4e5c6d055e95ae9f3f8ce63d2d0a0298eec57d8c
Kyle Oba <koba@mudphone.com>
2015-01-17 10:40:25 -1000

    Add blank Optionals B section.

---
+++ PLAYGROUNDS/LSB_B002_Optionals.playground/section-2.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+/*
+//  Optionals
+//
+//  Suggested Reading:
+//  http://www.objc.io/books/
+//    (Functional Programming in Swift, by Chris Eidhof, Florian Kugler, and Wouter Swierstra)
+/===================================*/
+
+
+
+/*------------------------------------/
+//  Map
+/------------------------------------*/
+
+
+
+
+
+
+
