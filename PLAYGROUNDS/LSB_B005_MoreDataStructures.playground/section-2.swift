84f15018016ac451c3e8bbe7cc86b041a9375561
Kyle Oba <koba@mudphone.com>
2015-01-18 01:06:01 -1000

    Add stub for a section on Tries.

---
+++ PLAYGROUNDS/LSB_B005_MoreDataStructures.playground/section-2.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+/*
+//  More Data Structures
+/===================================*/
+
+
+/*---------------------------------------------------------/
+//  Tries
+//
+//  What's a Trie?
+//
+//  In computer science, a trie, also called digital tree and sometimes radix tree or prefix tree (as they can be searched by prefixes), is an ordered tree data structure that is used to store a dynamic set or associative array where the keys are usually strings. Unlike a binary search tree, no node in the tree stores the key associated with that node; instead, its position in the tree defines the key with which it is associated. All the descendants of a node have a common prefix of the string associated with that node, and the root is associated with the empty string. Values are normally not associated with every node, only with leaves and some inner nodes that correspond to keys of interest. For the space-optimized presentation of prefix tree, see compact prefix tree.
+
+//  The term trie comes from re*trie*val. This term was coined by Edward Fredkin, who pronounces it /ˈtriː/ "tree" as in the word retrieval. However, other authors pronounce it /ˈtraɪ/ "try", in an attempt to distinguish it verbally from "tree".
+//  - Wikipedia (2015/01/18)
+//    https://en.wikipedia.org/wiki/Trie
+/---------------------------------------------------------*/
+
+
+
+
+
+
+
+
+
