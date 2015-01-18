24c68fecd6b413df38fc545b3dccebdce6412327
Kyle Oba <koba@mudphone.com>
2015-01-18 13:02:10 -1000

    Add trie insert method.

--- PLAYGROUNDS/LSB_B005_MoreDataStructures.playground/section-2.swift
+++ PLAYGROUNDS/LSB_B005_MoreDataStructures.playground/section-2.swift
@@ -2,6 +2,10 @@ import UIKit
 
 /*
 //  More Data Structures
+//
+//  Suggested Reading:
+//  http://www.objc.io/books/ (Chapter 9)
+//    (Functional Programming in Swift, by Chris Eidhof, Florian Kugler, and Wouter Swierstra)
 /===================================*/
 
 
@@ -15,9 +19,114 @@ import UIKit
 //  The term trie comes from re*trie*val. This term was coined by Edward Fredkin, who pronounces it /ˈtriː/ "tree" as in the word retrieval. However, other authors pronounce it /ˈtraɪ/ "try", in an attempt to distinguish it verbally from "tree".
 //  - Wikipedia (2015/01/18)
 //    https://en.wikipedia.org/wiki/Trie
+//
 /---------------------------------------------------------*/
 
+struct Trie<T: Hashable> {
+  typealias Children = [T: Trie<T>]
+  
+  var isElem: Bool
+  var children: Children
+}
 
+
+/*---------------------------------------------------------/
+//  Create an empty trie
+/---------------------------------------------------------*/
+func empty<T: Hashable>() -> Trie<T> {
+  return Trie(isElem: false, children: Trie.Children())
+}
+let emptyTrie: Trie<Character> = empty()
+
+
+/*---------------------------------------------------------/
+//  List trie elements
+/---------------------------------------------------------*/
+func elements<T: Hashable>(trie: Trie<T>) -> [[T]] {
+  var result: [[T]] = trie.isElem ? [[]] : []
+  for (key, value) in trie.children {
+    result += elements(value).map { xs in [key] + xs }
+  }
+  return result
+}
+
+elements(emptyTrie)
+
+
+/*---------------------------------------------------------/
+//  Required Array extension: Head-Tail
+/---------------------------------------------------------*/
+extension Array {
+  var decompose: (head: T, tail: [T])? {
+    return (count > 0) ? (self[0], Array(self[1..<count])) : nil
+  }
+}
+
+
+// Sum, as an exercise
+func sum(xs: [Int]) -> Int {
+  if let (head, tail) = xs.decompose {
+    return (head + sum(tail))
+  } else {
+    return 0
+  }
+}
+
+sum([])
+sum([42])
+sum([1, 3, 5])
+
+
+/*---------------------------------------------------------/
+//  Insert into trie
+/---------------------------------------------------------*/
+func trieInsert<T: Hashable>(key: [T], trie: Trie<T>) -> Trie<T> {
+  if let (head, tail) = key.decompose {
+    let isLast = key.count == 1
+    var trieCopy = trie
+    
+    // key found
+    if var subtrie = trie.children[head] {
+      if isLast { subtrie.isElem = true }
+      trieCopy.children[head] = trieInsert(tail, subtrie)
+    } else {
+      // key not found
+      var newSubtrie = Trie<T>(isElem: isLast, children: [T : Trie<T>]())
+      trieCopy.children[head] = trieInsert(tail, newSubtrie)
+    }
+    return trieCopy
+  } else {
+    // no more keys
+    return trie
+  }
+}
+
+// test with "cat"
+var root: Trie<Character> = empty()
+let cat = trieInsert(["c", "a", "t"], root)
+cat.isElem
+cat.children["c"]!
+cat.children["c"]!.isElem
+cat.children["c"]!.children["a"]!
+cat.children["c"]!.children["a"]!.isElem
+cat.children["c"]!.children["a"]!.children["t"]!
+cat.children["c"]!.children["a"]!.children["t"]!.isElem
+root.children
+
+// add another word, "car"
+let catr = trieInsert(["c", "a", "r"], cat)
+catr.isElem
+catr.children["c"]!
+catr.children["c"]!.isElem
+catr.children["c"]!.children["a"]!
+catr.children["c"]!.children["a"]!.isElem
+catr.children["c"]!.children["a"]!.children
+catr.children["c"]!.children["a"]!.children["r"]!
+catr.children["c"]!.children["a"]!.children["r"]!.isElem
+
+// "cat" remains element:
+catr.children["c"]!.children["a"]!.children["t"]!
+catr.children["c"]!.children["a"]!.children["t"]!.isElem
 
 
 
