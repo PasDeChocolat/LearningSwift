b7afac251fbf72d61d518532aa64ef2edb8514ee
Kyle Oba <koba@mudphone.com>
2015-01-29 14:53:24 -1000

    Add Haskell-y RPN calculation.

--- PLAYGROUNDS/LSB_D004_RPNCalculation.playground/section-2.swift
+++ PLAYGROUNDS/LSB_D004_RPNCalculation.playground/section-2.swift
@@ -82,7 +82,9 @@ class CalculatorBrain {
 
 
 
-// Test: 3, 4, +
+/*------------------------------------/
+//  Stanford: Test 3, 4, +
+/------------------------------------*/
 var calcBrain = CalculatorBrain()
 calcBrain.pushOperand(3)
 calcBrain.pushOperand(4)
@@ -94,23 +96,149 @@ func stringOperandToDouble(operand: String) -> Double {
   return NSNumberFormatter().numberFromString(operand)!.doubleValue
 }
 
-// The RPN Expression
+
+/*------------------------------------/
+//  Stanford: The RPN Expression
+/------------------------------------*/
 calcBrain = CalculatorBrain()
-//let expression = "10 4 3 + 2 * -"
 let expression = "10 4 3 + 2 × -"
 var ops = expression.componentsSeparatedByString(" ")
 for op in ops {
-  switch op {
-  case "×", "÷", "+", "-", "√":
+  if calcBrain.isKnownOp(op) {
     calcBrain.performOperation(op)
-  default:
+  } else {
     calcBrain.pushOperand(stringOperandToDouble(op))
   }
 }
 calcBrain.evaluate()!
 
 
+/*------------------------------------/
+//  The Haskell Way
+/------------------------------------*/
+func words(sentence: String) -> [String] {
+  return sentence.componentsSeparatedByCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+}
+words(expression)
+
+/*------------------------------------/
+//  Head : Tail
+/------------------------------------*/
+extension Array {
+  var decompose: (head: T, tail: [T])? {
+    return (count > 0) ? (self[0], Array(self[1..<count])) : nil
+  }
+  
+  var ddecompose: (head: T, htail: T, ttail: [T])? {
+    return (count > 1) ? (self[0], self[1], Array(self[2..<count])) : nil
+  }
+}
+
+var (head, htail, ttail) = [1, 2].ddecompose!
+head
+htail
+ttail
+ttail.first
+
+(head, htail, ttail) = [1, 2, 3, 4].ddecompose!
+head
+htail
+ttail
 
 
+/*------------------------------------------------------------------/
+//  These wouldn't be necessary if we could do more sophisticated
+//  pattern matching, as in Haskell.
+/------------------------------------------------------------------*/
+func performBinary(all: [Double], op: (Double, Double) -> Double) -> [Double] {
+  if var (x, y, ys) = all.ddecompose {
+    return [op(x, y)] + ys
+  }
+  assertionFailure("Could not perform binary op on: \(all)")
+}
+
+func performUnary(all: [Double], op: Double -> Double) -> [Double] {
+  if var (x, xs) = all.decompose {
+    return [op(x)] + xs
+  }
+  assertionFailure("Could not perform unary op on: \(all)")
+}
+
+
+/*------------------------------------------------------------------/
+//  Calculate expression via a left fold, placing either operands
+//  (converted to double), or operations (applied to operands from
+//  the accumulator), onto the accumulator.
+//
+//  The resulting accumulator value is the answer.
+/------------------------------------------------------------------*/
+func foldingFunction(all: [Double], op: String) -> [Double] {
+  switch op {
+  case "×":
+    return performBinary(all, *)
+  case "+":
+    return performBinary(all, +)
+  case "-":
+    return performBinary(all) { $1 - $0 }
+  case "÷":
+    return performBinary(all) { $1 / $0 }
+    case "√":
+      return performUnary(all, sqrt)
+  default:
+    return [stringOperandToDouble(op)] + all
+  }
+}
+
+func solveRPN(expression: String) -> Double {
+  let words = expression.componentsSeparatedByCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+  return words.reduce([], foldingFunction).first!
+}
+
+solveRPN(expression)
+
+
+/*----------------------------------------/
+//  Attempt at a More Haskelly Solution
+//
+//  My apologies to people who actually
+//  know how to write Haskell gud.
+/----------------------------------------*/
+
+/*------------------------------------/
+//  Head: HTail: TTail
+/------------------------------------*/
+extension Array {
+  var ddecomp: (head: T?, htail: T?, ttail: [T]) {
+    switch self.count {
+    case 0: return (nil, nil, self)
+    case 1: return (self[0], nil,     [])
+    case 2: return (self[0], self[1], [])
+    case _: return (self[0], self[1], Array(self[2..<count]))
+    }
+  }
+}
+
+
+func foldingFunction2(all: [Double], op: String) -> [Double] {
+  switch (all.ddecomp, op) {
+  case (let (x, y, ys), "×"):
+    return [x! * y!] + ys
+  case (let (x, y, ys), "+"):
+    return [x! + y!] + ys
+  case (let (x, y, ys), "-"):
+    return [y! - x!] + ys
+  case (let (x, y, ys), "÷"):
+    return [y! / x!] + ys
+  case (let (x, y, ys), "√"):
+    return [sqrt(x!)] + (y != nil ? [y!] + ys : ys)
+  default:
+    return [stringOperandToDouble(op)] + all
+  }
+}
+
+
+words("1 2 3 4").reduce([], foldingFunction2)
+words(expression).reduce([], foldingFunction2).first!
+words("4 √ 3 × 4 + 1 - √").reduce([], foldingFunction2).first!
 
 
