53b61db172d0c9fb5ef7df18f16a2121d8630eea
Kyle Oba <koba@mudphone.com>
2015-02-04 08:17:25 -1000

    Add parser combinators through applicative functor's <*> and <|> operator.

--- PLAYGROUNDS/LSB_B011_ParserCombinators.playground/section-2.swift
+++ PLAYGROUNDS/LSB_B011_ParserCombinators.playground/section-2.swift
@@ -37,7 +37,7 @@ extension Slice {
   }
   
   var decompose: (head: T, tail: Slice<T>)? {
-    return self.isEmpty ? nil : (self[self.startIndex], self.tail)
+    return self.isEmpty ? nil : (self.head!, self.tail)
   }
 }
 
@@ -68,6 +68,16 @@ struct Parser<Token, Result> {
 }
 
 
+// Structs get an init() method for free, that will take the properties
+// as arguments. So, since Parser only has one property, which stores a
+// function, the trailing closure syntax acts as an initializer.
+let nullParser1: Parser<Character, Character> = Parser { _ in none() }
+
+
+// This is equivalent, but long-winded
+let nullParser2: Parser<Character, Character> = Parser(p: { _ in none() })
+
+
 /*---------------------------------------------------------------------/
 //  Simple example: Just parse single character "a"
 /---------------------------------------------------------------------*/
@@ -83,6 +93,20 @@ func parseA() -> Parser<Character, Character> {
   }
 }
 
+
+// Let's inspect the results of applying a Parser
+typealias ParseToken = Character
+typealias Remaining = Slice<Character>
+typealias ParsePair = (ParseToken, Remaining)
+let parserA: Parser<Character, Character> = parseA()
+let sequenceParseA: SequenceOf<ParsePair> = parserA.p("abcd".slice)
+var generatorParseA: GeneratorOf<ParsePair> = sequenceParseA.generate()
+let resultParseA: ParsePair? = generatorParseA.next()
+resultParseA!.0
+resultParseA!.1
+
+
+// Let's automate Parser testing
 func testParser<A>(parser: Parser<Character, A>, input: String) -> String {
     var result: [String] = []
     for (x, s) in parser.p(input.slice) {
@@ -110,6 +134,39 @@ func parseCharacter(character: Character) -> Parser<Character, Character> {
 }
 
 testParser(parseCharacter("t"), "test")
+
+
+/*---------------------------------------------------------------------/
+//  Make generic over any kind of token
+/---------------------------------------------------------------------*/
+func satisfy<Token>(condition: Token -> Bool) -> Parser<Token, Token> {
+  return Parser { x in
+    if let (head, tail) = x.decompose {
+      if condition(head) {
+        return one((head, tail))
+      }
+    }
+    return none()
+  }
+}
+
+
+// This is better
+let charZ: Character = "z"
+let parserZ = satisfy { $0 == charZ }
+testParser(parserZ, "zed")
+testParser(parserZ, "abc")
+
+
+// But we can make it shorter
+func token<Token: Equatable>(t: Token) -> Parser<Token, Token> {
+  return satisfy { $0 == t }
+}
+
+
+// Syntactic Sugar for a Parser for the "x" character
+testParser(token("x"), "xyz")
+testParser(token("a"), "xyz")
 
 
 /*---------------------------------------------------------------------/
@@ -160,27 +217,7 @@ func +<A>(l: SequenceOf<A>, r: SequenceOf<A>) -> SequenceOf<A> {
 
 
 /*---------------------------------------------------------------------/
-//  Make generic over any kind of token
-/---------------------------------------------------------------------*/
-func satisfy<Token>(condition: Token -> Bool) -> Parser<Token, Token> {
-  return Parser { x in
-    if let (head, tail) = x.decompose {
-      if condition(head) {
-        return one((head, tail))
-      }
-    }
-    return none()
-  }
-}
-
-
-func token<Token: Equatable>(t: Token) -> Parser<Token, Token> {
-  return satisfy { $0 == t }
-}
-
-
-/*---------------------------------------------------------------------/
-//  Choice operator
+//  Choice operator - Combining multiple parsers
 /---------------------------------------------------------------------*/
 infix operator <|> { associativity right precedence 130 }
 func <|> <Token, A>(l: Parser<Token, A>,
@@ -194,6 +231,7 @@ func <|> <Token, A>(l: Parser<Token, A>,
 testParser(token("a") <|> token("b"), "bcd")
 testParser(token("b") <|> token("a"), "bcd")
 testParser(token("a") <|> token("x"), "bcd")
+testParser(token("a") <|> token("b") <|> token("c"), "cde")
 
 
 /*---------------------------------------------------------------------/
@@ -231,16 +269,185 @@ let parserXY: Parser<Character, (Character, Character)> = sequence(token("x"), t
 testParser(parserXY, "xyz")
 
 
+// And shorter
+let a: Character = "a"
+let b: Character = "b"
+let parserAB = sequence(token(a), token(b))
+testParser(parserAB, "abc")
+
+
+// Let's pick this apart
+let sequenceParseAB = parserAB.p("abc".slice)
+var generatorParseAB = sequenceParseAB.generate()
+let resultParseAB = generatorParseAB.next()
+resultParseAB!      // ((a, b), [c])
+resultParseAB!.0    //  (a, b)
+resultParseAB!.1    //          [c]
+resultParseAB!.0.0  //   a
+resultParseAB!.0.1  //      b
+
+
+/*---------------------------------------------------------------------/
+//  Sequence more Parsers
+/---------------------------------------------------------------------*/
+// You could do it brute force, but this doesn't scale.
+func sequence3<Token, A, B, C>(p1: Parser<Token, A>,
+                               p2: Parser<Token, B>,
+                               p3: Parser<Token, C>)
+                               -> Parser<Token, (A, B, C)> {
+    
+    return Parser { input in
+      let p1Results = p1.p(input)
+      return flatMap(p1Results) { a, p1Rest in
+        let p2Results = p2.p(p1Rest)
+        return flatMap(p2Results) { b, p2Rest in
+          let p3Results = p3.p(p2Rest)
+          return map(p3Results, { c, p3Rest in
+            ((a, b, c), p3Rest)
+          })
+        }
+      }
+    }
+}
+
+let c: Character = "c"
+let p3 = sequence3(token(a), token(b), token(c))
+testParser(p3, "abcd")
+
+
 /*---------------------------------------------------------------------/
 //  Sequence the Parsers: The refined way
 /---------------------------------------------------------------------*/
 
+// Doesn't consume any tokens, returns a function that just returns an
+// integer conversion of its input
+func integerParser<Token>() -> Parser<Token, Character -> Int> {
+  return Parser { input in
+    return one(({ x in String(x).toInt()! }, input))
+  }
+}
+
+let intParser: Parser<Character, Character -> Int> = integerParser()
+var generatorIntParser = intParser.p("74".slice).generate()
+let (intFunc, rest) = generatorIntParser.next()!
+intFunc("9")  // converts to Int
+rest          // nothing consumed
 
 
+// The Combinator
+func combinator<Token, A, B>(l: Parser<Token, A -> B>,
+                             r: Parser<Token, A>)
+                             -> Parser<Token, B> {
+    
+    return Parser { input in
+      let leftResults = l.p(input)
+      return flatMap(leftResults) { f, leftRemainder in
+        let rightResults = r.p(leftRemainder)
+        return map(rightResults) { x, rightRemainder in
+          (f(x), rightRemainder)
+        }
+      }
+    }
+}
+
+// The Combinator in action
+let three: Character = "3"
+testParser(combinator(integerParser(), token(three)), "3")
+
+let combinatorParser = combinator(integerParser(), token(three))
+let combinatorSeq = combinatorParser.p("34".slice)
+var combinatorGen = combinatorSeq.generate()
+let combinatorResult = combinatorGen.next()
+combinatorResult!
+combinatorResult!.0 == 3
 
 
+/*---------------------------------------------------------------------/
+//  Pure - Returns a value in a default context
+//  pure :: a -> f a
+/---------------------------------------------------------------------*/
+func pure<Token, A>(value: A) -> Parser<Token, A> {
+  return Parser { one((value, $0)) }
+}
+
+let pureA: Parser<Character, Character> = pure(a)
+let seqPureA = pureA.p("abc".slice)
+var genPureA = seqPureA.generate()
+let resultPureA = genPureA.next()
+resultPureA!
+resultPureA!.0
+resultPureA!.1
 
 
+// Always returns the value
+testParser(pureA, "x")
+
+
+// Can now wrap a simple predicate in `pure`
+func toInteger(c: Character) -> Int {
+  return String(c).toInt()!
+}
+testParser(combinator(pure(toInteger), token(three)), "3")
+
+
+// Due to currying, can go through the combination process multiple times
+func toInteger2(c1: Character)(c2: Character) -> Int {
+  let combined = String(c1) + String(c2)
+  return combined.toInt()!
+}
+testParser(combinator(combinator(pure(toInteger2), token(three)),
+  token(three)), "33")
+
+
+/*---------------------------------------------------------------------/
+//  <*>
+//
+//  “for Maybe, <*> extracts the function from the left value if it’s 
+//   a Just and maps it over the right value. If any of the parameters
+//   is Nothing, Nothing is the result.”
+//   - Excerpt From: Miran Lipovaca. “Learn You a Haskell for Great Good!.” iBooks.
+/---------------------------------------------------------------------*/
+infix operator <*> { associativity left precedence 150 }
+func <*><Token, A, B>(l: Parser<Token, A -> B>,
+                      r: Parser<Token, A>)
+                      -> Parser<Token, B> {
+  return combinator(l, r)
+}
+
+
+// Using the new operator
+testParser(pure(toInteger2) <*> token(three) <*> token(three), "33")
+
+
+/*---------------------------------------------------------------------/
+//  <|>
+//
+//  Combines several existing strings
+/---------------------------------------------------------------------*/
+func string(characters: [Character]) -> String {
+  var s = ""
+  s.extend(characters)
+  return s
+}
+
+let aOrB = token(a) <|> token(b)
+
+func combine(a: Character)(b: Character)(c: Character) -> String {
+  return string([a, b, c])
+}
+
+let parser = pure(combine) <*> aOrB <*> aOrB <*> token(b)
+testParser(parser, "abb")
+
+
+// With our own currying for three parameter functions
+func curry<A, B, C, D>(f: (A, B, C) -> D) -> A -> B -> C -> D {
+  return { a in { b in { c in f(a, b, c) } } }
+}
+
+let parser2 = pure(curry { string([$0, $1, $2]) })
+              <*> aOrB <*> aOrB <*> token(b)
+testParser(parser2, "abb")
 
 
 
